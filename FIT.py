3409c768b34acab6cc331d297b876ee1fa669535
Xiaoyu Xu <xuxiaoyu200@gmail.com>
2026-01-30 10:19:53 +0800

    Update and rename main.py to FIT.py

--- FIT.py
+++ FIT.py
@@ -27,15 +27,16 @@ from Fit.filtering import Unlearn_request_memory, Data_filtering
 from Fit.layer import LayerAttributionSelector
 
 TensorLike = Union[torch.Tensor, List[torch.Tensor]]
+
+def cubic_root(x):
+    sign = 1 if x >= 0 else -1
+    return sign * (abs(x) ** (1/3))
+
 def log_results(
     log_file,
     user_id,
-    forget_probability_score,
-    forget_rouge,
-    forget_acc,
-    retain_probability_score,
-    retain_rouge,
-    retain_acc,
+    FD,
+    RQ
 ):
     """
     Record log data into a file.
@@ -43,20 +44,14 @@ def log_results(
     Args:
       log_file: Path to the log file. Must end with .json
       user_id: User ID.
-      forget_probability_score: Forget probability score.
-      forget_rouge: ROUGE score on the forget set.
-      retain_probability_score: Retain probability score.
-      retain_rouge: ROUGE score on the retain set.
-      retain_acc: Token-level accuracy on the retain set.
-      mmlu_acc: Accuracy on MMLU dataset.
-      gsm8k_acc: Accuracy on GSM8K dataset.
-      commonsense_acc: Accuracy on commonsense dataset.
+      Forget Degree: Forget Degree.
+      Retain Quality: Retain Quality.
     """
     # Construct the record data dictionary
     log_data = {
         "user_id": user_id,
-        "forget_probability_score": forget_probability_score,
-        "forget_rouge": forget_rouge,
+        "Forget Degree": FD,
+        "Retain Quality": RQ,
     }
     
     # If the file does not exist, create a new file and write data (stored as a list for later appending)
@@ -74,7 +69,7 @@ def log_results(
             f.seek(0)  # Move the file pointer to the beginning
             json.dump(existing_data, f, indent=4, ensure_ascii=False)
 
-def Unlearning_Evaluator(user_id, model_name, model, tokenizer, forget_set, retain_set, forget_question, retain_question, forget_answer, retain_answer, batch_size, max_length, log_result_path, device):
+def Unlearning_Evaluator(model, tokenizer, forget_set, retain_set, forget_question, retain_question, forget_answer, retain_answer, batch_size, max_length, device):
     # Probability
     forget_probability_score=Probability(model, tokenizer, forget_question, forget_answer, batch_size)
     retain_probability_score=Probability(model, tokenizer, retain_question, retain_answer, batch_size)
@@ -84,21 +79,81 @@ def Unlearning_Evaluator(user_id, model_name, model, tokenizer, forget_set, reta
     retain_rouge=Rouge_L(model, tokenizer, retain_question, retain_answer, batch_size, max_length)
 
     # Accuracy
-    retain_acc,forget_acc=Accuracy(model, tokenizer, forget_set, retain_set, batch_size)
+    forget_acc, retain_acc = Accuracy(model, tokenizer, forget_set, retain_set, batch_size)
 
     print("forget set \n")
     print(f"prob={forget_probability_score:.4f}, rougeL={forget_rouge:.4f}, acc={forget_acc:.4f}")
     print("retain set \n")
     print(f"prob={retain_probability_score:.4f}, rougeL={retain_rouge:.4f}, acc={retain_acc:.4f}")
-    log_results(log_result_path, 
-                user_id,
-                forget_probability_score,
-                forget_rouge,
-                forget_acc,
-                retain_probability_score,
-                retain_rouge,
-                retain_acc,
-                )
+    Forget = cubic_root(forget_probability_score * forget_rouge * forget_acc)*100
+    Retain = cubic_root(retain_probability_score * retain_rouge * retain_acc)*100
+    return Forget, Retain
+
+# Linear interpolation function
+def linear_interp_values(y_start, y_end, xs=(60, 120, 180, 240, 300), x0=0, x1=300):
+    """
+    y(x) = a*x + b, where (x0, y_start) and (x1, y_end) are endpoints.
+    Return {x: y(x)} for x in xs.
+    """
+    if x1 == x0:
+        raise ValueError("x1 must be different from x0")
+    a = (y_end - y_start) / (x1 - x0)
+    b = y_start - a * x0
+    return {x: a * x + b for x in xs}
+#Compute FQ and RQ curves
+def compute_forget_retain_Q(
+                            unlearning_model_name: str,
+                            retain_model_name: str,
+                            forget_set,
+                            retain_set,
+                            forget_question, 
+                            retain_question, 
+                            forget_answer, 
+                            retain_answer,
+                            batch_size: int,
+                            max_length: int,
+                            device: str):
+    # 1) load model/tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(retain_model_name)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+    retain_model = AutoModelForCausalLM.from_pretrained(
+        retain_model_name,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        use_flash_attention_2= True,
+    ).to(device)
+    unlearning_model = AutoModelForCausalLM.from_pretrained(
+        unlearning_model_name,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=True,
+        use_flash_attention_2= True,
+    ).to(device)
+    retain_model.eval()
+    unlearning_model.eval()
+    Forget_Q_start,Retain_Q_start=Unlearning_Evaluator(unlearning_model, tokenizer, forget_set, retain_set, forget_question, retain_question, forget_answer, retain_answer, batch_size, max_length, device)
+    Forget_Q_end,Retain_Q_end=Unlearning_Evaluator(retain_model, tokenizer, forget_set, retain_set, forget_question, retain_question, forget_answer, retain_answer, batch_size, max_length, device)
+    Forget_Q_curve = linear_interp_values(Forget_Q_start, Forget_Q_end)
+    Retain_Q_curve = linear_interp_values(Retain_Q_start, Retain_Q_end)
+    del retain_model
+    del unlearning_model
+    del tokenizer
+    gc.collect()
+    torch.cuda.empty_cache()
+    print("Forget_Q:", Forget_Q_curve)
+    print("Retain_Q:", Retain_Q_curve)
+    return Forget_Q_curve, Retain_Q_curve
+
+# Calculate FD and RQ
+def FD_RQ_calculate(unlearn_list, retain_list):
+    out = []
+    for A, B in zip(unlearn_list, retain_list):
+        if B == 0:
+            out.append(0)
+        else:
+            out.append(max(0.0, 1.0 - abs(A / B - 1.0)))
+    return out
 
 # ======= Main Pipeline: Comprehensive Example =======
 class UnlearningPipeline:
@@ -205,6 +260,7 @@ def parse_args():
     
     # Core hyperparameters
     parser.add_argument("--unlearning_model_name",type=str,default="FITPCH/Llama-2-7b-chat-hf_PCH_finetune", help="unlearning model names")
+    parser.add_argument("--retain_model_name",type=str,default="FITPCH/Llama-2-7b-chat-hf_PCH_retain", help="retain model names")
     parser.add_argument("--topk_for_forget", type=int, default=8,
                         help="Number of top-k layers to select for forgetting")
     parser.add_argument("--similarity_threshold", type=float, default=0.90,
@@ -213,6 +269,8 @@ def parse_args():
                         help="Epsilon threshold for Data_filtering")
     parser.add_argument("--device", type=str, default="cuda:0",
                         help="Device to use for training (e.g., 'cuda:0' or 'cpu')")
+    return parser.parse_args()
+
 # ====================== Test Example ======================
 if __name__ == "__main__":
     # Set random seed (ensure reproducibility)
@@ -235,52 +293,71 @@ if __name__ == "__main__":
     device = args.device  # Device to use for training
     epsilon = args.epsilon  # Epsilon for Data_filtering
     Unlearning_model_name=args.unlearning_model_name
+    retain_model_name=args.retain_model_name
+    # Compute FQ and RQ curves
+    Forget_Q_list,Retain_Q_list=compute_forget_retain_Q(Unlearning_model_name, retain_model_name, Pch[0:300], Pch[300:], Pch_Q[0:300],Pch_Q[300:], Pch_A[0:300], Pch_A[300:], batch_size, max_length, device)
     Unlearning_model_file_name=Unlearning_model_name.replace("/", "_")
     seed_list=[20,30,40,50] 
     # Initialize main Pipeline
     for m in range(len(seed_list)):
         random.seed(seed_list[m])
         # Shuffle lists
-        random.shuffle(Pch)
-        random.shuffle(Pch_Q)
-        random.shuffle(Pch_A)
-        for j in range(len(Unlearning_model_name)):
-            pipeline = UnlearningPipeline(
-                target_name=Unlearning_model_name[j],
-                forget_data=Pch[0:300],
-                all_forget_retain_data=Pch,
-                similarity_threshold=similarity_threshold,
-                Data_filtering_chunk_size=Data_filtering_chunk_size,
-                device=device,  # Use "cuda" if GPU is available
-                epsilon=epsilon
+        for L in (Pch, Pch_Q, Pch_A):
+            tmp = L[:300]
+            random.shuffle(tmp)
+            L[:300] = tmp
+        Forget_list=[]
+        Retain_list=[]
+        pipeline = UnlearningPipeline(
+            target_name=Unlearning_model_name,
+            forget_data=Pch[0:300],
+            all_forget_retain_data=Pch,
+            similarity_threshold=similarity_threshold,
+            Data_filtering_chunk_size=Data_filtering_chunk_size,
+            device=device,  # Use "cuda" if GPU is available
+            epsilon=epsilon
+        )
+        log_result_path=f"Experiment_record/continuous/{Unlearning_model_file_name}/seed{seed_list[m]}_evaluations_all.json"
+        for i in range(300):
+            user_id="User"+str(i)
+            model,tokenizer,all_chosen_layers_list,delete_list= pipeline.unlearning_request_handler(user_id, Pch[i], Pch[i+1:], max_length, topk_for_forget, fine_tuning_chunk_size, lr, epochs)
+            if (i+1) % 60 == 0:
+                #Calculate F and R
+                Forget,Retain = Unlearning_Evaluator(model, tokenizer, Pch[0:i+1], Pch[i+1:], Pch_Q[0:i+1], Pch_Q[i+1:], Pch_A[0:i+1], Pch_A[i+1:], batch_size, max_length, device)
+                Forget_list.append(Forget)
+                Retain_list.append(Retain)
+        print("Forget_list:", Forget_list)
+        print("Retain_list:", Retain_list)
+        #Calculate FQ and RQ
+        User_id=[60,120,180,240,300]
+        FD_list=FD_RQ_calculate(Forget_list,Forget_Q_list)
+        RQ_list=FD_RQ_calculate(Retain_list,Retain_Q_list)
+        log_result_path=f"Experiment_record/continuous/{Unlearning_model_file_name}/seed{seed_list[m]}_evaluations_all.json"
+        # Log results for FD and RQ for each 60 users
+        for idx in range(len(User_id)):
+            log_results(
+                log_file=log_result_path,
+                user_id=User_id[idx],
+                FD=FD_list[idx],
+                RQ=RQ_list[idx]
             )
-            log_result_path=f"Experiment_record/continuous/{Unlearning_model_file_name}/seed{seed_list[m]}_evaluations_all.json"
-            for i in range(300):
-                user_id="User"+str(i)
-                model,tokenizer,all_chosen_layers_list,delete_list= pipeline.unlearning_request_handler(user_id, Pch[i], Pch[i+1:], max_length, topk_for_forget, fine_tuning_chunk_size, lr, epochs)
-                if (i+1) % 60 == 0:
-                    Unlearning_Evaluator(user_id, Unlearning_model_name[j], model, tokenizer, Pch[0:i+1], Pch[i+1:], Pch_Q[0:i+1], Pch_Q[i+1:], Pch_A[0:i+1], Pch_A[i+1:], batch_size, max_length, log_result_path,device)
-            print(f"Training time: {end_time - start_time:.2f} seconds")
-            # Figure 1: Retain Accuracy vs Forget Requests  
-
-            # # Save model weights
-            # # Check if folder exists, if not create it
-            with open(f"Experiment_record/continuous/{Unlearning_model_file_name}/seed{seed_list[m]}_comparison.json", "w", encoding="utf-8") as f:
-                json.dump(delete_list, f, ensure_ascii=False, indent=4)
-            model_save_path=f"Model/continuous/{Unlearning_model_file_name}/seed{seed_list[m]}"
-            if not os.path.exists(model_save_path):
-                os.makedirs(model_save_path)
-            model.save_pretrained(model_save_path)
-            tokenizer.save_pretrained(model_save_path)
-            del model
-            del tokenizer
-            # Force garbage collection
-            gc.collect()
-
-            # Clear PyTorch GPU cache
-            torch.cuda.empty_cache()
-            # Clear PyTorch IPC cache
-            torch.cuda.ipc_collect()
+        # # Save model weights
+        # # Check if folder exists, if not create it
+        with open(f"Experiment_record/continuous/{Unlearning_model_file_name}/seed{seed_list[m]}_comparison.json", "w", encoding="utf-8") as f:
+            json.dump(delete_list, f, ensure_ascii=False, indent=4)
+        model_save_path=f"Model/continuous/{Unlearning_model_file_name}/seed{seed_list[m]}"
+        if not os.path.exists(model_save_path):
+            os.makedirs(model_save_path)
+        model.save_pretrained(model_save_path)
+        tokenizer.save_pretrained(model_save_path)
+        del model
+        del tokenizer
+        # Force garbage collection
+        gc.collect()
+        # Clear PyTorch GPU cache
+        torch.cuda.empty_cache()
+        # Clear PyTorch IPC cache
+        torch.cuda.ipc_collect()
 
 
 
